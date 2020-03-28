60045284a9b833dec37845d884add99e7935827e
uo225824 <christiaapin06-gmail.com>
2020-03-28 02:27:26 +0100

    Second commit

--- ejemploregresionfuncional.R
+++ ejemploregresionfuncional.R
@@ -1,12 +1,16 @@
+#Modelo teórico para la regresión con respuesta funcional
+library(fda.usc)
 set.seed(12345)
 t = seq(0, 1, length = nt <- 100)
 covexp = function(t1, t2) {
   3 * exp(-abs(t1 - t2)/0.5)
 }
 Sigma = outer(t, t, covexp)
+#X Proceso Gaussiano con covarianza Sigma
 X = rproc2fdata(n <- 200, t, sigma = Sigma)
 Y<-matrix(0,nrow = 200,ncol = 100)
 
+#Nucleo del operador integral
 beta<-matrix(0,ncol = 100,nrow = 100)
 t<-seq(0,1,length.out = 100)
 s<-seq(0,1,length.out = 100)
@@ -17,6 +21,7 @@ for(i in 1:100){
   
 }
 
+#Residuo como combinación de senos y cosenos
 epsilon<- matrix(0,200,100)
 m<-199
 for(i in 1:100){
@@ -24,6 +29,8 @@ for(i in 1:100){
 }
 
 Y<-X$data%*%beta/100+epsilon
+
+#Base optima
 opt.bsY<-optim.basis(fdata(Y),type.basis = "bspline")
 opt.bsX<-optim.basis(X,type.basis = "bspline")
 
@@ -37,6 +44,7 @@ datosfN<-Data2fd(t(Y),seq(0,1,length=100),basisobj = basisy)
 lambdas<-1
 lambdat<-1
 
+#Estimación rapida del orden de la penalización
 par(mfrow=c(3,4))
 for (i in 1:10) {
   lambdas<-lambdas/10
@@ -50,20 +58,16 @@ for (i in 1:10) {
   
   }
 
-phi=9/4*(0:m/m)%*%t(0:m/m)
-# True surface evaluated on a discrete bivariate grid
-par(mar=c(1.5, 1.5, 3.5, 0.2), mfrow=c(2,2), oma = c(4, 4, 0.2
-                                                     , 0.2))
-par(mfrow=c(2,2))
-# 4 panels 2 rows and 2 columns arrangement
-axislabelsize=1.5 # controls the size of labels of axes
-axisticksize=0.8 # controls the size of ticks of axes
-persp((0:99/99),(0:199/199),z=beta,cex.axis = axisticksize,
+par(mfrow=c(1,2))
+axislabelsize=1.5 
+axisticksize=0.8 
+#Modelo teorico
+persp((0:99/99),(0:99/99),z=beta,cex.axis = axisticksize,
       cex.lab=axislabelsize, xlab="t",ylab="s", zlab=" ", theta=30,
       phi=30,
       ticktype="detailed", main="Valor teórico de Beta(t,s)",col = "blue")
-
-persp((0:30/30),(0:30/30),z=res.fr$beta.estbifd$coefs,cex.axis = axisticksize,
+#Modelo estimado
+persp((0:44/44),(0:5/5),z=res.fr$beta.estbifd$coefs,cex.axis = axisticksize,
       cex.lab=axislabelsize, xlab="t",ylab="s", zlab=" ", theta=30,
       phi=30,
       ticktype="detailed", main="True",col = "blue")
